62995d7465cd015d98d28d8e79a3b83cdc74ffcb
Sedat Barlin <barlinsedat@gmail.com>
2024-06-12 16:25:43 +0300

    working on it

---
+++ FrameworksApp/FrameworksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksAppApp.swift
+//  FrameworksApp
+//
+//  Created by Sedat on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
