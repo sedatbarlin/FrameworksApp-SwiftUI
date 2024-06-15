de03d72802688407895a9f37389cacd86f9ab74f
Sedat Barlin <barlinsedat@gmail.com>
2024-06-15 18:59:09 +0300

    working on it

--- FrameworksApp/FrameworkGridView.swift
+++ FrameworksApp/FrameworkGridView.swift
@@ -11,10 +11,16 @@ struct FrameworkGridView: View {
     let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
-        LazyVGrid(columns: columns){
-            ForEach(MockData.frameworks){ framework in
-                FrameworkTitleView(framework: framework)
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(MockData.frameworks){ framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
             }
+            
+            .navigationTitle("Apple Frameworks")
         }
     }
 }
@@ -37,5 +43,6 @@ struct FrameworkTitleView: View {
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
+        .padding()
     }
 }
