acc958865bd14c2849a73e4690b6f76eb2deac95
Sedat Barlin <barlinsedat@gmail.com>
2024-06-15 19:01:26 +0300

    working on it

---
+++ FrameworksApp/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  FrameworksApp
+//
+//  Created by Sedat on 15.06.2024.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
