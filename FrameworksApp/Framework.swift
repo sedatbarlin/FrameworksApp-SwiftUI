f31c3568e2c76540af34c3ee977211b8f7ee9881
Sedat Barlin <barlinsedat@gmail.com>
2024-06-23 00:54:50 +0300

    Update Framework.swift

--- FrameworksApp/Framework.swift
+++ FrameworksApp/Framework.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Framework: Hashable, Identifiable{
     var id = UUID() 
-    let name: String
+    let name: String 
     let imageName: String
     let urlString: String
     let description: String
