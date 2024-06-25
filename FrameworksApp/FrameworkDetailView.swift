55efe555cf406dcd155cc0d0e388a4ea7f5288f2
Sedat Barlin <barlinsedat@gmail.com>
2024-06-25 10:28:37 +0300

    working on it

--- FrameworksApp/FrameworkDetailView.swift
+++ FrameworksApp/FrameworkDetailView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct FrameworkDetailView: View {
+    var framework: Framework
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Spacer()
+                Button{
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                }
+            }
+            .padding()
+            Spacer()
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Spacer()
+            Button{
+                
+            } label: {
+                 AFButton(title: "Learn More")
+            }
+        }
     }
 }
 
 #Preview {
-    FrameworkDetailView()
+    FrameworkDetailView(framework: MockData.sampleFramework)
 }
