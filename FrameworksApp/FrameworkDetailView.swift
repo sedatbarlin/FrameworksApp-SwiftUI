a332d09a3cb324fd757ced723d4eed117983b237
Sedat Barlin <barlinsedat@gmail.com>
2024-06-25 14:52:52 +0300

    working on it

--- FrameworksApp/FrameworkDetailView.swift
+++ FrameworksApp/FrameworkDetailView.swift
@@ -9,19 +9,12 @@ import SwiftUI
 
 struct FrameworkDetailView: View {
     var framework: Framework
+    @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
+    
     var body: some View {
         VStack{
-            HStack{
-                Spacer()
-                Button{
-                    
-                } label: {
-                    Image(systemName: "xmark")
-                        .foregroundColor(Color(.label))
-                        .imageScale(.large)
-                }
-            }
-            .padding()
+            XDismissButton(isShowingDetailView: $isShowingDetailView)
             Spacer()
             FrameworkTitleView(framework: framework)
             Text(framework.description)
@@ -29,14 +22,17 @@ struct FrameworkDetailView: View {
                 .padding()
             Spacer()
             Button{
-                
+                isShowingSafariView = true
             } label: {
                  AFButton(title: "Learn More")
             }
         }
+        .fullScreenCover(isPresented: $isShowingSafariView, content: {
+            SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
+        })
     }
 }
 
 #Preview {
-    FrameworkDetailView(framework: MockData.sampleFramework)
+    FrameworkDetailView(framework: MockData.sampleFramework, isShowingDetailView: .constant(false))
 }
--- FrameworksApp/FrameworkGridView.swift
+++ FrameworksApp/FrameworkGridView.swift
@@ -8,41 +8,29 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
-    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
+    @StateObject var viewModel = FrameworkGridViewModel()
     
     var body: some View {
         NavigationView{
             ScrollView{
-                LazyVGrid(columns: columns){
+                LazyVGrid(columns: viewModel.columns){
                     ForEach(MockData.frameworks){ framework in
                         FrameworkTitleView(framework: framework)
+                            .onTapGesture {
+                                viewModel.selectedFramework = framework
+                            }
                     }
                 }
             }
-            
             .navigationTitle("Apple Frameworks")
+            .sheet(isPresented: $viewModel.isShowingDetailView){
+                FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFramework, isShowingDetailView: $viewModel.isShowingDetailView)
+            }
         }
     }
 }
 
 #Preview {
     FrameworkGridView()
-}
-
-struct FrameworkTitleView: View {
-    var framework: Framework
-    
-    var body: some View {
-        VStack{
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-        }
-        .padding()
-    }
 }
